f2f3dd892748dc33e02a54b412d4af00a6536c1f
小鑫同学 <zxin088@gmail.com>
2023-09-03 15:40:30 +0800

    update

---
+++ docs/.vitepress/views/project-list.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="container">
+    <div class="layout-page">
+      <h3 class="project-tier">Projects</h3>
+      <div class="project-grid">
+        <div
+          class="project-grid-item"
+          v-for="project in projects.values"
+          :key="project.pLink"
+        >
+          <a class="project-grid-link" :href="project.pLink" target="_blank">
+            <div class="project-box">
+              <div class="project-wrap">
+                <img class="project-icon" :src="project.pIcon" />
+                <h2 class="project-name">{{ project.pName }}</h2>
+              </div>
+              <div class="project-details">{{ project.pDetails }}</div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from "vue";
+const projects = reactive({
+  values: [
+    {
+      pIcon: "https://github.githubassets.com/favicons/favicon.png",
+      pName: "cross-platform-components",
+      pLink: "https://github.com/OSpoon/cross-platform-components",
+      pDetails: "您可以基于本工程构建跨技术栈/无框架的组件，满足个性化需求。",
+    },
+    {
+      pIcon: "https://github.githubassets.com/favicons/favicon.png",
+      pName: "var-conv",
+      pLink: "https://github.com/OSpoon/var-conv",
+      pDetails: "一款适用于VSCode IDE的代码变量名称快速转换工具",
+    },
+    {
+      pIcon: "https://github.githubassets.com/favicons/favicon.png",
+      pName: "generator-vite-plugin",
+      pLink: "https://github.com/OSpoon/generator-vite-plugin",
+      pDetails: "基于Yeoman的Vite插件生成器。",
+    },
+  ],
+});
+</script>
+<style scoped>
+.container {
+  padding: 24px;
+}
+
+.layout-page {
+  margin: 0 auto;
+  max-width: 1152px;
+}
+
+.project-tier {
+  border-radius: 16px 16px 0 0;
+  padding: 13px 0 11px;
+  font-size: 14px;
+  margin-bottom: 4px;
+  text-align: center;
+  letter-spacing: 1px;
+  line-height: 24px;
+  width: 100%;
+  font-weight: 600;
+  color: var(--vp-c-text-2);
+  background-color: var(--vp-c-bg-soft);
+  margin: 35px auto 20px auto;
+}
+
+.project-grid {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0;
+  margin: -8px;
+}
+
+.project-grid-item {
+  padding: 8px;
+  background-color: transparent;
+  transition: none;
+}
+
+.project-grid-link {
+  height: 158px;
+  display: flex;
+  background-color: var(--vp-c-bg-soft);
+  transition: background-color 0.25s;
+}
+
+.project-box {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+}
+
+.project-wrap {
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+.project-icon {
+  width: 32px;
+  height: 32px;
+  filter: grayscale(1);
+  transition: filter 0.25s;
+}
+.project-name {
+  font-size: 18px;
+  font-weight: 500;
+  margin-left: 10px;
+}
+
+.project-details {
+  padding: 0 15px;
+  padding-top: 8px;
+  font-size: 14px;
+  font-weight: 500;
+  color: var(--vp-c-text-2);
+}
+
+@media (min-width: 640px) {
+  .container {
+    padding: 0 48px;
+  }
+  .project-grid-item {
+    width: calc(100% / 2);
+  }
+}
+
+@media (min-width: 768px) {
+  .project-grid-item {
+    width: calc(100% / 2);
+  }
+}
+
+@media (min-width: 960px) {
+  .container {
+    padding: 0 64px;
+  }
+
+  .project-grid-item {
+    width: calc(100% / 2);
+  }
+}
+
+@media (min-width: 1024px) {
+  .project-grid-item {
+    width: calc(100% / 3);
+  }
+}
+</style>
--- docs/index.md
+++ docs/index.md
@@ -4,7 +4,7 @@ layout: home
 
 hero:
   name: "前端小鑫同学"
-  text: "🏅InfoQ签约作者"
+  text: "🏆InfoQ签约作者"
   tagline: 专注前端技术布道，所有付出都将是沉淀，所有美好终会如期而至~
   image:
     src: /logo.png
@@ -14,19 +14,30 @@ hero:
       text: 我的博客
       link: /blog/readme
     - theme: alt
-      text: 系列开发教程
+      text: 🎉 系列开发教程
       link: /document/readme
 
 features:
-  - title: 💡 梦想
+  - icon: 💡
+    title: 编程梦想
     details: 因一次编写自动登录游戏脚本而产生编程的兴趣，顾万物皆有裨益，万事皆可为师。
-  - title: 🏗 经历
+  - icon: 🏗
+    title: 成长经历
     details: 从一本厚重的《疯狂 Android 讲义》启蒙到应环境变化做一位前端程序员心路历程。
-  - title: 🔑 方式
+  - icon: 🔑
+    title: 学习方式
     details: 繁多的技术不可能靠记忆实现，做笔记和写博客是记录学习过程和分享学习成果的捷径。
-  - title: 📦 沉淀
+  - icon: 📦
+    title: 如何沉淀
     details: 选择在 1024Code 平台以卡片的形式记录每一个案例，以集合的形式串联一个系列。
 ---
+
+
+<script lang="ts" setup>
+import ProjectList from '../docs/.vitepress/views/project-list.vue';
+</script>
+
+<project-list></project-list>
 
 <style>
 :root {
